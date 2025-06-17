0e85a99eb18151e7707c15700f91b82d2f8762f9
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-06-17 14:53:07 +0600

    Initial Commit

---
+++ TaskMate/TaskMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMateApp.swift
+//  TaskMate
+//
+//  Created by Akram Ul Hasan on 17/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
