7410219b8b7ecb1f032bad519f4532e442bfbf39
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-07-03 13:49:33 +0600

    Add task list screen

---
+++ TaskMate/Screen/TMAddTaskListScreen/TMAddTaskListScreen.swift
@@ -0,0 +1,52 @@
+//
+//  TMAddTaskListScreen.swift
+//  TaskMate
+//
+//  Created by Akram Ul Hasan on 2/7/25.
+//
+
+import SwiftUI
+
+struct TMAddTaskListScreen: View {
+    
+    @EnvironmentObject private var coordinator : AppCoordinator
+    @EnvironmentObject private var db : TMDatabaseManager
+    @State private var taskListTitle = ""
+    
+    func saveTaskList() {
+        db.createTaskList(title: taskListTitle)
+    }
+    
+    var body: some View {
+        VStack {
+            TextField("Enter list title", text: $taskListTitle)
+                .font(.headline)
+                .foregroundStyle(.blackLevel1)
+                .padding(.horizontal, 20)
+                .padding(.vertical, 40)
+                .background(.whiteLevel1)
+            
+            Spacer()
+            
+                .navigationTitle("Create new list")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Done") {
+                            saveTaskList()
+                            coordinator.pop()
+                        }
+                        .disabled(taskListTitle.isEmpty)
+                    }
+                }
+        }
+        .background(.whiteLevel2)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        TMAddTaskListScreen()
+        
+    }
+}
